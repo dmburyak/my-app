f8914bf90ef35cafbb8fb936ef40626ab94d4efa
Dmitri <d.buryak@gmail.com>
2021-04-29 17:03:28 +0300

    add DropdownDirective

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -19,4 +19,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+  }
 }
